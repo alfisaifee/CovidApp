ecf98adc70450c50a27b845959b1513c854b531a
Alfia Saifee <alfiamoiz@hotmail.com>
2020-06-15 07:50:41 +0000

    added bootstrap css to pagination

--- ClientApp/src/components/CovidData.js
+++ ClientApp/src/components/CovidData.js
@@ -7,7 +7,7 @@ export class CovidData extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { covidInfo: [], loading: true, activePage: 1, pageSize: 10};
+        this.state = { covidInfo: [], loading: true, activePage: 1, pageSize: 10 };
     }
 
     componentDidMount() {
@@ -15,56 +15,62 @@ export class CovidData extends Component {
     }
 
     handlePageChange = (page) => {
-        this.setState({ activePage : page })
+        this.setState({ activePage: page })
     }
 
     static renderCovidTable(covidInfo, activePage, pageSize) {
         const covidInfoPage = paginate(covidInfo, activePage, pageSize)
         return (
             <div>
-            <table className='table table-striped' aria-labelledby="table-label">
-                <thead>
-                    <tr>
-                        <th>Country</th>
-                        <th>Recovered Cases</th>
-                        <th>Deaths</th>
-                        <th>Total Cases</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
+                <table className='table table-striped' aria-labelledby="table-label">
+                    <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>Recovered Cases</th>
+                            <th>Deaths</th>
+                            <th>Total Cases</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
                             covidInfoPage.map(info =>
-                            <tr key={info.countryCode}>
-                                <td>{info.countryCountry}</td>
-                                <td>{info.totalRecovered}</td>
-                                <td>{info.totalDeaths}</td>
-                                <td>{info.totalConfirmed}</td>
-                            </tr>
-                        )}
-                </tbody>
+                                <tr key={info.countryCode}>
+                                    <td>{info.countryCountry}</td>
+                                    <td>{info.totalRecovered}</td>
+                                    <td>{info.totalDeaths}</td>
+                                    <td>{info.totalConfirmed}</td>
+                                </tr>
+                            )}
+                    </tbody>
                 </table>
             </div>
-                );
+        );
     }
 
     static renderPagination(covidInfo, handlePageChange, activePage, pageSize) {
         return (
-        <Pagination
-            totalItemsCount={covidInfo.length}
-            itemsCountPerPage={pageSize}
-            onChange={handlePageChange}
-            activePage={activePage}
-            pageRangeDisplayed={5}>
-        </Pagination>);
+            <Pagination
+                totalItemsCount={covidInfo.length}
+                itemsCountPerPage={pageSize}
+                onChange={handlePageChange}
+                activePage={activePage}
+                pageRangeDisplayed={5}
+                itemClass="page-item"
+                linkClass="page-link"
+                prevPageText="prev"
+                nextPageText="next"
+            >
+            </Pagination>);
     }
 
     render() {
-        let contents = this.state.loading ? <p><em>Loading...</em></p>
-            : CovidData.renderCovidTable(this.state.covidInfo, this.handlePageChange, this.state.activePage, this.state.pageSize);
+        let contents = this.state.loading ? <p><em>Loading...</em></p> :
+            CovidData.renderCovidTable(this.state.covidInfo, this.state.activePage, this.state.pageSize);
+
         const paginate = CovidData.renderPagination(this.state.covidInfo, this.handlePageChange, this.state.activePage, this.state.pageSize);
+
         return (
             <div>
-                <h4 id="tableLabel">Covid Information</h4>
                 <p id="tableLabel">{}</p>
                 <p>The below table displays the information of covid-19 cases.</p>
                 {contents}
