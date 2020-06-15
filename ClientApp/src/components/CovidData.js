c9a909cbb07bc3750d4467a6ce838f6d5b542e35
Alfia Saifee <alfiamoiz@hotmail.com>
2020-06-15 06:28:01 +0000

    added pagination react-js-pagination

---
+++ ClientApp/src/components/CovidData.js
@@ -0,0 +1,81 @@
+﻿import React, { Component } from 'react';
+import Pagination from 'react-js-pagination';
+import { paginate } from '../utils/paginate';
+
+export class CovidData extends Component {
+    static displayName = CovidData.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { covidInfo: [], loading: true, activePage: 1, pageSize: 10};
+    }
+
+    componentDidMount() {
+        this.populateCovidData();
+    }
+
+    handlePageChange = (page) => {
+        this.setState({ activePage : page })
+    }
+
+    static renderCovidTable(covidInfo, activePage, pageSize) {
+        const covidInfoPage = paginate(covidInfo, activePage, pageSize)
+        return (
+            <div>
+            <table className='table table-striped' aria-labelledby="table-label">
+                <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th>Recovered Cases</th>
+                        <th>Deaths</th>
+                        <th>Total Cases</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                            covidInfoPage.map(info =>
+                            <tr key={info.countryCode}>
+                                <td>{info.countryCountry}</td>
+                                <td>{info.totalRecovered}</td>
+                                <td>{info.totalDeaths}</td>
+                                <td>{info.totalConfirmed}</td>
+                            </tr>
+                        )}
+                </tbody>
+                </table>
+            </div>
+                );
+    }
+
+    static renderPagination(covidInfo, handlePageChange, activePage, pageSize) {
+        return (
+        <Pagination
+            totalItemsCount={covidInfo.length}
+            itemsCountPerPage={pageSize}
+            onChange={handlePageChange}
+            activePage={activePage}
+            pageRangeDisplayed={5}>
+        </Pagination>);
+    }
+
+    render() {
+        let contents = this.state.loading ? <p><em>Loading...</em></p>
+            : CovidData.renderCovidTable(this.state.covidInfo, this.handlePageChange, this.state.activePage, this.state.pageSize);
+        const paginate = CovidData.renderPagination(this.state.covidInfo, this.handlePageChange, this.state.activePage, this.state.pageSize);
+        return (
+            <div>
+                <h4 id="tableLabel">Covid Information</h4>
+                <p id="tableLabel">{}</p>
+                <p>The below table displays the information of covid-19 cases.</p>
+                {contents}
+                {paginate}
+            </div>
+        );
+    }
+
+    async populateCovidData() {
+        const response = await fetch('api/coviddata');
+        const data = await response.json();
+        this.setState({ covidInfo: data, loading: false });
+    }
+}
---
+++ Controllers/CovidDataController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyFirstApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CovidDataController : ControllerBase
+    {
+        [HttpGet]
+        public IList<Country> Get()
+        {
+            CovidData covidData = new CovidData();
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.covid19api.com/")
+            };
+
+            HttpResponseMessage response = client.GetAsync("summary").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                covidData = JsonConvert.DeserializeObject<CovidData>(response.Content.ReadAsStringAsync().Result);
+            }
+            else
+            {
+                Debug.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            IList<Country> countries = covidData.Countries;
+            return countries;
+        }
+    }
+}
